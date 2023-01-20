5fbc8a9fd0bdc5e6041f04a1b2ed94cd0f506fd0
chai2222 <chaitanayajagadale6@gmail.com>
2023-01-20 20:39:18 +0530

    BasicRe

---
+++ User/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace User
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to REGEX User Registration Problem");
+        Start:
+            Console.WriteLine("Select Options Please :\n" +
+                "1)Enter Sample Emails\n");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+
+                    UserRegistration user8 = new UserRegistration();
+                    Console.WriteLine(":::::::::::::::::::::::::::::;::");
+                    user8.ValidSampleEmail();
+                    break;
+
+
+
+                default:
+                    Console.WriteLine("Choose Proper options");
+                    break;
+            }
+            goto Start;
+        }
+    }
+}
---
+++ User/UserRegistration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace User
+{
+    internal class UserRegistration
+    {
+        public void ValidSampleEmail()
+        {
+            Console.WriteLine("Please Enter Sample Email :");
+            string data = Console.ReadLine();
+
+            string pattern = "^[a-zA-Z0-9]+([#$%+_-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]{1,}[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$";
+
+            //creating object of the Regex superclass
+
+            Regex regobj = new Regex(pattern);
+
+            if (regobj.IsMatch(data))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Sample Email is Valid");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Sample Email is Invalid");
+                Console.ResetColor();
+            }
+        }
+    }
+}
