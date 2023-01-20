712795634d7ea44fecdf0c0664d202c6972347a1
chai2222 <chaitanayajagadale6@gmail.com>
2023-01-20 20:53:42 +0530

    CheckPassword

--- User/Program.cs
+++ User/Program.cs
@@ -8,10 +8,7 @@
             Console.WriteLine("Welcome to REGEX User Registration Problem");
         Start:
             Console.WriteLine("Select Options Please :\n" +
-                "1)Enter First Name\n" +
-                "2)Enter Last Name\n" +
-                "3)Enter E-Mail Address\n" +
-                "4)Enter Phone Number\n");
+                 "1)Enter Password Rule No. 1\n");
 
             int option = Convert.ToInt32(Console.ReadLine());
 
@@ -19,29 +16,9 @@
             {
                 case 1:
 
-                    UserRegistration user = new UserRegistration();
-                    Console.WriteLine(":::::::::::::::::::::::::::::::");
-                    user.ValidFirstName();
-                    break;
-
-                case 2:
-
-                    UserRegistration user1 = new UserRegistration();
-                    Console.WriteLine(":::::::::::::::::::::::::::::::");
-                    user1.ValidLastName();
-                    break;
-
-                case 3:
-                    UserRegistration user2 = new UserRegistration();
+                    UserRegistration user4 = new UserRegistration();
                     Console.WriteLine(":::::::::::::::::::::::::::::;::");
-                    user2.ValidEmail();
-                    break;
-
-                case 4:
-
-                    UserRegistration user3 = new UserRegistration();
-                    Console.WriteLine(":::::::::::::::::::::::::::::;::");
-                    user3.ValidPhNumber();
+                    user4.ValidPasswordRule1();
                     break;
 
 
--- User/UserRegistration.cs
+++ User/UserRegistration.cs
@@ -4,15 +4,14 @@ using System.Text.RegularExpressions;
 
 namespace User
 {
-    public class UserRegistration
+    public  class UserRegistration
     {
-        //UC1
-        public void ValidFirstName()
+        public void ValidPasswordRule1()
         {
-            Console.WriteLine("Please Enter First Name :");
+            Console.WriteLine("Please Enter Password Rule 1 :");
             string data = Console.ReadLine();
 
-            string pattern = "^[A-Z][a-z]{2,10}$";
+            string pattern = "^[a-zA-Z]{8,}$";
 
             //creating object of the Regex superclass
 
@@ -21,93 +20,13 @@ namespace User
             if (regobj.IsMatch(data))
             {
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("First Name is Valid");
+                Console.WriteLine("Password Rule 1 is Valid");
                 Console.ResetColor();
             }
             else
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("First Name is Invalid");
-                Console.ResetColor();
-            }
-        }
-
-        //UC2
-        public void ValidLastName()
-        {
-            Console.WriteLine("Please Enter Last Name :");
-            string data = Console.ReadLine();
-
-            string pattern = "^[A-Z][a-z]{2,12}$";
-
-            //creating object of the Regex superclass
-
-            Regex regobj = new Regex(pattern);
-
-            if (regobj.IsMatch(data))
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Last Name is Valid");
-                Console.ResetColor();
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Last Name is Invalid");
-                Console.ResetColor();
-            }
-        }
-
-        //UC3
-
-        public void ValidEmail()
-        {
-            Console.WriteLine("Please Enter Email :");
-            string data = Console.ReadLine();
-
-            string pattern = "^[a-zA-z]{3,}([.][a-zA-Z]{3,})[@][a-z]{2}[.][a-z]{2}([.][a-z]{2})$";
-
-            //creating object of the Regex superclass
-
-            Regex regobj = new Regex(pattern);
-
-            if (regobj.IsMatch(data))
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Email is Valid");
-                Console.ResetColor();
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Email is Invalid");
-                Console.ResetColor();
-            }
-        }
-
-        //UC4
-
-        public void ValidPhNumber()
-        {
-            Console.WriteLine("Please Enter Phone Number :");
-            string data = Console.ReadLine();
-
-            string pattern = "^[0-9]{2}[ ][6-9]{1}[0-9]{9}$";
-
-            //creating object of the Regex superclass
-
-            Regex regobj = new Regex(pattern);
-
-            if (regobj.IsMatch(data))
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Phone Number is Valid");
-                Console.ResetColor();
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Phone Number is Invalid");
+                Console.WriteLine("Password Rule 1 Invalid");
                 Console.ResetColor();
             }
         }
